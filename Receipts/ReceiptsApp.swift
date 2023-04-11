72c3da037b563de786ca2f5dfeb8ae31a3f9b87b
Ben Dodson <ben@bendodson.com>
2023-04-11 10:18:16 +0100

    Initial Commit

---
+++ Receipts/ReceiptsApp.swift
@@ -0,0 +1,12 @@
+// Developed by Ben Dodson (ben@bendodson.com)
+
+import SwiftUI
+
+@main
+struct ReceiptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
